1a359058d2dc287f5b998a65a83cde9687e75095
zhujunjie <zhujunjie@qq.com>
2020-06-29 19:28:21 +0800

    register complete

--- client/js/register.js
+++ client/js/register.js
@@ -1,4 +1,5 @@
-$(() => {
+$(()=>{
+
     let imgCode;
     /*不传值，统一走默认值*/
     let captcha = new Captcha({
@@ -18,7 +19,73 @@ $(() => {
     captcha.draw(document.querySelector('#captcha'), r => {
         console.log('验证码', r);
         imgCode = r;
+
         /* 自动触发标签的事件 */
         $("#code").trigger("blur");
     });
-});
+    
+    //事件委托验证
+    let options = {
+        "usernameID":{
+            reg:`/^[a-zA-z]{2,6}$/.test(val)`,
+            msg:"用户名不符合规范！"
+        },
+        "phoneID":{
+            reg:`/^1[3-9][0-9]{9}$/.test(val)`,
+            msg:"手机号码不符合规范！"
+        },
+        "pwdAID":{
+            reg:`/^[a-zA-z0-9]{6,8}$.test(val)/`,
+            msg:"密码不符合规范！"
+        },
+        "pwdBID":{
+            reg:`val===$.trim($("#pwdAID").val())`,
+            msg:"请两次密码保持一致"
+        },
+        "code": {
+            reg: `val === imgCode`,
+            msg: "图形验证码不正确！"
+        }
+    }
+    $(".main_con input").blur(function() {
+        let option_id = this.id;
+        console.log("option_id", options[option_id]);
+
+        let val = $.trim($(this).val());
+
+        if (eval(options[option_id].reg)) {
+            $(this).next().text("");
+        } else {
+            $(this).next().text(options[option_id].msg);
+        }
+    })
+    //    注册功能(获取参数并且发送网络请求， 在服务器端进行处理)
+      $("#registerBtn").click(function() {
+        /* [1] 检查表单验证是否全部都通过，如果有一个没有通过那么就return  */
+        $("#phoneID,#usernameID,#pwdAID,#pwdBID,#code").trigger("blur");
+        //判断是否勾选
+        let isCheck = $("#protocol").is(":checked");
+        if (!isCheck) {
+            alert("请阅读并同意用户的注册协议!!!");
+            return;
+        }
+        let data = {
+            username: $.trim($("#usernameID").val()),
+            phone: $.trim($("#phoneID").val()),
+            password: ($.trim($("#pwdAID").val()))
+        }
+          $.ajax({
+            url: "../../server/res.php",
+            type: "post",
+            data,
+            dataType: "json",
+        }).done(data => {
+            if (data.status == "success") {
+                alert("注册成功!");
+                location.href = "../html/login.html";
+            } else {
+                alert(data.msg);
+            }
+        })
+      })
+})
