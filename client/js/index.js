b777576ec21ebf280d3821db4a2e36b90f1ffe97
zhujunjie <zhujunjie@qq.com>
2020-06-30 22:56:37 +0800

    Slider complete

---
+++ client/js/index.js
@@ -0,0 +1,11 @@
+$(()=>{
+    $(".menu").mouseenter(function(){
+        $(this).children(".icon-jiantouxia").css("display","none");
+        $(this).children(".icon-jiantoushang").css("display","block");
+
+    })
+    $(".menu").mouseleave(function(){
+        $(this).children(".icon-jiantouxia").css("display","block");
+        $(this).children(".icon-jiantoushang").css("display","none");
+    })
+})
